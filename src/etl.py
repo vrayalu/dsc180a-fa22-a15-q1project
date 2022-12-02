c182b102b9670a1820a9a88379b2796a17c6777e
Freddy <f5wang@ucsd.edu>
2022-12-02 14:54:01 -0800

    download complete

---
+++ src/etl.py
@@ -0,0 +1,32 @@
+import os
+import requests
+import networkx as nx
+import pandas as pd
+
+def get_data():
+   
+    url = "http://www.casos.cs.cmu.edu/computational_tools/datasets/external/polblogs/polblogs.gml"
+    response = requests.get(url)
+
+    directory = "data/"
+    parent_dir = "./"
+    filename = "polblogs.gml"
+    path = os.path.join(parent_dir, directory)
+    file_path = os.path.join(path, filename)
+
+    os.makedirs(path)
+    file = open(file_path, "wb")
+    file.write(response.content)
+
+def edit_graphtype(filepath):
+    with open(filepath, 'r') as file :
+        filedata = file.read()
+    filedata = filedata.replace('directed 1', 'multigraph 1')
+    with open(filepath, 'w') as file:
+        file.write(filedata)
+
+def read_graph(filepath):
+    G = nx.read_gml(filepath)
+    nodes = pd.DataFrame.from_dict(dict(G.nodes(data=True)), orient='index')
+    truth = dict(zip(nodes.index,nodes.value))
+    return G, truth
